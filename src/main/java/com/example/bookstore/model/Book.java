c6eb4934742519c04951ae41e3e5890595e29f72
bhq638@myy.haaga-helia.fi <bhq638@myy.haaga-helia.fi>
2025-09-01 08:24:34 +0300

    ensimmäinen commit

---
+++ src/main/java/com/example/bookstore/model/Book.java
@@ -0,0 +1,53 @@
+package com.example.bookstore.model;
+
+public class Book {
+
+    String title;
+    String author;
+    int publicationYear;
+    int isbn;
+    double price;
+
+    public Book(){
+        
+    }
+    public Book(String title, String author, int publicationYear, int isbn, double price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public int getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
