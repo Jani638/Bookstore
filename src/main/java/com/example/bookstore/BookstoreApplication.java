bc464c5d2298155377949d6407662f8f3d69454a
bhq638@myy.haaga-helia.fi <bhq638@myy.haaga-helia.fi>
2025-10-27 16:54:22 +0200

    Testaus commit Herokua varten

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -39,10 +39,12 @@ public class BookstoreApplication {
             //repository.save(new Book("Clean Code", "Robert C. Martin", 2008, "192-3876-749", 42.50, software));
         //}
 
-		//AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "user@email.com", "USER");
-		//AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "admin@email.com", "ADMIN");
-		//urepository.save(user1);
-		//urepository.save(user2);
+		AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "user@email.com", "USER");
+		AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "admin@email.com", "ADMIN");
+		urepository.save(user1);
+		urepository.save(user2);
+
+		
 
 		log.info("fetch all books");
 			for (Book book : repository.findAll()) {
